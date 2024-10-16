939e58b3021d6aac01bcc276eac27098d7572885
inhooo00 <chldlsgh0987@naver.com>
2024-10-16 09:11:01 +0900

    five

--- src/World.java
+++ src/World.java
@@ -2,4 +2,9 @@ public class World {
     public static void run() {
         System.out.println("202014117");
     }
+
+    public static void run2() {
+        System.out.println("hello world");
+    }
+
 }
